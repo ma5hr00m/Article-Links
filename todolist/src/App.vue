6f8d7a48b9b52c72e64de2f64c953c723c2882d0
ma5hr00m <3411281455@qq.com>
2023-03-24 23:47:35 +0800

    update

---
+++ todolist/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>TodoList</h1>
+  <TodoList />
+</template>
+
+<script>
+import TodoList from "@/components/TodoList.vue";
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+
+<style>
+#app{
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  height: 600px;
+  width: 800px;
+}
+
+h1:before{
+  content: "<";
+}
+h1:after{
+  content: ">";
+}
+</style>
---
+++ todolist/src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="search-box">
+    <button type="submit">search</button>
+    <input type="text" id="search" name="search" placeholder="Input keywords...">
+  </div>
+  <div id="list-box">
+    <ul>
+      <li>
+        <label for="checker">
+          <input type="checkbox" class="checker">
+          <span>This is a task</span>
+        </label>
+      </li>
+    </ul>
+  </div>
+  <div id="create-box">
+    <button type="submit">create</button>
+    <input type="text" id="create" name="create" placeholder="Creata an item...">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList"
+}
+</script>
+
+<style scoped>
+ul{
+  margin: 20px 20px 20px 0;
+  padding: 0;
+  list-style: none;
+}
+.checker{
+  margin-right: 10px;
+}
+</style>
